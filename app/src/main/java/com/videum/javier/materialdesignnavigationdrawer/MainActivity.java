3a5c69f18163ccc831421b393fcc052bdb8fe509
Javier Segovia Córdoba <thedahnark@gmail.com>
2015-01-29 18:20:45 +0100

    Initial commit

---
+++ app/src/main/java/com/videum/javier/materialdesignnavigationdrawer/MainActivity.java
@@ -0,0 +1,121 @@
+package com.videum.javier.materialdesignnavigationdrawer;
+
+import android.content.res.Configuration;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    FrameLayout statusBar;
+    Toolbar toolbar;
+    ActionBarDrawerToggle drawerToggle;
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Cast toolbar and status bar
+        statusBar = (FrameLayout) findViewById(R.id.statusBar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        // Get support to the toolbar and change its title
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Main Activity");
+
+        // Setup Navigation drawer
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        View drawer = findViewById(R.id.scrimInsetsFrameLayout);
+
+        // Fix right margin to 56dp (portrait)
+        ViewGroup.LayoutParams layoutParams = drawer.getLayoutParams();
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            layoutParams.width = displayMetrics.widthPixels - (56 * Math.round(displayMetrics.density));
+        }
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            layoutParams.width = displayMetrics.widthPixels + (20 * Math.round(displayMetrics.density)) - displayMetrics.widthPixels / 2;
+        }
+
+        // Setup Drawer Icon
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.setDrawerListener(drawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        drawerToggle.syncState();
+
+        // Fix portrait issues
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            // Fix issues for KitKat setting Status Bar color primary
+            if (Build.VERSION.SDK_INT >= 19) {
+                TypedValue typedValue19 = new TypedValue();
+                MainActivity.this.getTheme().resolveAttribute(R.attr.colorPrimary, typedValue19, true);
+                final int color = typedValue19.data;
+                FrameLayout statusBar = (FrameLayout) findViewById(R.id.statusBar);
+                statusBar.setBackgroundColor(color);
+            }
+
+            // Fix issues for Lollipop, setting Status Bar color primary dark
+            if (Build.VERSION.SDK_INT >= 21) {
+                TypedValue typedValue21 = new TypedValue();
+                MainActivity.this.getTheme().resolveAttribute(R.attr.colorPrimaryDark, typedValue21, true);
+                final int color = typedValue21.data;
+                FrameLayout statusBar = (FrameLayout) findViewById(R.id.statusBar);
+                statusBar.setBackgroundColor(color);
+            }
+        }
+
+        // Fix landscape issues (only Lollipop)
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            if (Build.VERSION.SDK_INT >= 19) {
+                TypedValue typedValue19 = new TypedValue();
+                MainActivity.this.getTheme().resolveAttribute(R.attr.colorPrimary, typedValue19, true);
+                final int color = typedValue19.data;
+                FrameLayout statusBar = (FrameLayout) findViewById(R.id.statusBar);
+                statusBar.setBackgroundColor(color);
+            }
+            if (Build.VERSION.SDK_INT >= 21) {
+                TypedValue typedValue = new TypedValue();
+                MainActivity.this.getTheme().resolveAttribute(R.attr.colorPrimaryDark, typedValue, true);
+                final int color = typedValue.data;
+                drawerLayout.setStatusBarBackgroundColor(color);
+            }
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
